01ecb5f8e2ad08ef5001b8d761f8a50eb333c995
podhmo <ababjam61+github@gmail.com>
2020-11-29 06:37:24 +0900

    feat: first prototype

---
+++ handler.go
@@ -0,0 +1,30 @@
+package inflexible
+
+import (
+	"context"
+	"io"
+)
+
+type HandlerFunc func(context.Context, Event) (interface{}, error)
+
+type getter interface {
+	Get(string) string
+}
+
+type Event struct {
+	Name    string
+	Headers getter // TODO: refine
+	Body    io.ReadCloser
+}
+
+// -- context
+
+type key string
+
+const (
+	keyEvent key = ":event"
+)
+
+func WithEvent(ctx context.Context, ev Event) context.Context {
+	return context.WithValue(ctx, keyEvent, ev)
+}
---
+++ weblib/weblib.go
@@ -0,0 +1,43 @@
+package weblib
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/podhmo/inflexible"
+	"github.com/podhmo/tenuki"
+)
+
+// TODO: rename
+func LiftHandler(h inflexible.HandlerFunc) http.HandlerFunc {
+	name := fmt.Sprintf("%v", h) // todo: fix
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			t := recover()
+			if t != nil {
+				log.Printf("hmm %+v", t)
+				tenuki.Render(w, r).JSON(500, map[string]string{"message": fmt.Sprintf("%s", t)})
+			}
+		}()
+
+		// TODO:headers and queries
+		ev := inflexible.Event{
+			Name:    name,
+			Body:    r.Body,
+			Headers: r.URL.Query(), // + headers
+		}
+
+		ctx := inflexible.WithEvent(r.Context(), ev)
+		result, err := h(ctx, ev)
+		if err != nil {
+			code := 500
+			if x, ok := err.(interface{ StatusCode() int }); ok {
+				code = x.StatusCode()
+			}
+			tenuki.Render(w, r).JSON(code, map[string]string{"message": err.Error()})
+			return
+		}
+		tenuki.Render(w, r).JSON(200, result)
+	}
+}
